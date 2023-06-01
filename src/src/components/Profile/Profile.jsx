8b92e4da1d1ae2aa8e18a9fd9eeafd76cb7131ad
valeri143 <valeria23200523251605@gmail.com>
2023-06-01 09:31:43 +0300

    fixed3

--- src/src/components/Profile/Profile.jsx
+++ src/src/components/Profile/Profile.jsx
@@ -37,10 +37,10 @@ return(
 
 Profile.propTypes ={
     username: PropTypes.string.isRequired,
-tag: PropTypes.string.isRequired,
-location: PropTypes.string.isRequired,
-avatar: PropTypes.string.isRequired,
-stats: PropTypes.exact({
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.exact({
     followers: PropTypes.number.isRequired,
     views: PropTypes.number.isRequired,
     likes: PropTypes.number.isRequired,
